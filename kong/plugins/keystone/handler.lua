23d5d9f1c7562c43930464ace37c8ed03c321b03
bura2017 <valeriya.shvetcova@phystech.edu>
2017-10-27 10:17:35 +0300

    review

--- kong/plugins/keystone/handler.lua
+++ kong/plugins/keystone/handler.lua
@@ -6,4 +6,10 @@ function KeystoneHandler:new()
   KeystoneHandler.super.new(self, "keystone")
 end
 
+function KeystoneHandler:access(config)
+  KeystoneHandler.super.access(self)
+
+  print(config.token_expiration) -- {"apikey"}
+end
+
 return KeystoneHandler
